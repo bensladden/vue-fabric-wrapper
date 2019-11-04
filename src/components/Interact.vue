b2274d26878f048ca548fea1bfd059d38fd910b0
bensladden <ben.sladden@dematec.com.au>
2019-11-04 21:19:55 +1030

    Initial Commit

---
+++ src/components/Interact.vue
@@ -0,0 +1,19 @@
+<template></template>
+
+<script>
+import interact from "interactjs";
+export default {
+  name: "Intreract",
+  props: {
+    draggable: Boolean,
+    resizable: Boolean
+  },
+  methods: {},
+  mounted() {},
+  watch: {},
+  beforeDestroy() {}
+};
+</script>
+
+<style>
+</style>
