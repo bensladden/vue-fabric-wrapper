eb8ce0a42a04be67d52faca7c6333189c47555c9
bensladden <ben.sladden@dematec.com.au>
2019-11-09 20:01:59 +1030

    modified home page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Vue-Fabric</h1>
     <fabric-canvas>
       <fabric-rectangle :id="1"></fabric-rectangle>
       <fabric-circle :id="2"></fabric-circle>
