7a02779c2fcabda3b6ebde1f5558bc1d4047cc21
bensladden <ben.sladden@dematec.com.au>
2019-11-10 20:09:31 +1030

    Added build lib scripts

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import FabricCanvas from "./FabricCanvas.vue";
+import FabricCircle from "./FabricCircle.vue";
+import FabricEllipse from "./FabricEllipse.vue";
+import FabricImageFromURL from "./FabricImageFromURL.vue";
+import FabricRectangle from "./FabricRectangle.vue";
+import FabricTriangle from "./FabricTriangle.vue";
+
+// import ResponsiveGridLayout from './ResponsiveGridLayout.vue';
+
+const VueFabric = {
+    FabricCanvas,
+    FabricCircle,
+    FabricEllipse,
+    FabricImageFromURL,
+    FabricRectangle,
+    FabricTriangle
+};
+
+Object.keys(VueFabric).forEach(name => {
+    Vue.component(name, VueFabric[name]);
+});
+
+export default VueFabric;
+export { FabricCanvas, FabricCircle, FabricEllipse, FabricImageFromURL, FabricRectangle, FabricTriangle };
