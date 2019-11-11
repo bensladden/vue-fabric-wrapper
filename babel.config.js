472c9e78a6b739bbaa6d9a7fe5b8dc58b15bedc1
bensladden <ben.sladden@dematec.com.au>
2019-11-11 19:55:49 +1030

    dont use polyfills in lib

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
+const isLib = process.env.VUE_CLI_BUILD_TARGET === "lib";
+
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+    presets: [
+        [
+            "@vue/cli-plugin-babel/preset",
+            {
+                useBuiltIns: isLib ? false : "usage"
+            }
+        ]
+    ]
 };
