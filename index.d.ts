078f7fe1daab2d92ca867f91a5e50d502bea73e1
bensladden <ben.sladden@dematec.com.au>
2020-02-20 10:49:30 +1030

    started definitions

---
+++ index.d.ts
@@ -0,0 +1,41 @@
+import Vue, { Component } from "vue";
+
+export class FabricAnimation extends Vue {}
+
+export class FabricBackgroundImage extends Vue {}
+
+export class FabricCanvas extends Vue {}
+
+export class FabricCircle extends Vue {}
+
+export class FabricDotGrid extends Vue {}
+
+export class FabricEllipse extends Vue {}
+
+export class FabricGradient extends Vue {}
+
+export class FabricGroup extends Vue {}
+
+export class FabricImageFromURL extends Vue {}
+
+export class FabricLine extends Vue {}
+
+export class FabricLineGrid extends Vue {}
+
+export class FabricOverlayImage extends Vue {}
+
+export class FabricPath extends Vue {}
+
+export class FabricPolygon extends Vue {}
+
+export class FabricPolyLine extends Vue {}
+
+export class FabricRectangle extends Vue {}
+
+export class FabricShadow extends Vue {}
+
+export class FabricSVGFromURL extends Vue {}
+
+export class FabricText extends Vue {}
+
+export class FabricTriangle extends Vue {}
